a19ea94506062f0e8dc6805003a217396aebe23a
likehomeanim <likehomeanimalosterrescue@gmail.com>
2024-11-24 17:13:11 -0600

    Add files via upload

---
+++ wp-content/uploads/uag-plugin/assets/0/uag-js-275.js
@@ -0,0 +1,13 @@
+document.addEventListener("DOMContentLoaded", function(){ window.addEventListener( 'load', function() {
+	UAGBCounter.init( '.uagb-block-3f062709', {"layout":"number","heading":"Lives Transformed","numberPrefix":"","numberSuffix":"+","startNumber":0,"endNumber":5000,"totalNumber":5000,"decimalPlaces":0,"animationDuration":1500,"thousandSeparator":",","circleSize":230,"circleStokeSize":8,"isFrontend":true} );
+});
+window.addEventListener( 'load', function() {
+	UAGBCounter.init( '.uagb-block-42fde678', {"layout":"number","heading":"Meals Distributed","numberPrefix":"","numberSuffix":"+","startNumber":0,"endNumber":10000,"totalNumber":10000,"decimalPlaces":0,"animationDuration":1500,"thousandSeparator":",","circleSize":230,"circleStokeSize":8,"isFrontend":true} );
+});
+window.addEventListener( 'load', function() {
+	UAGBCounter.init( '.uagb-block-21045de4', {"layout":"number","heading":"Students Sponsored","numberPrefix":"","numberSuffix":"+","startNumber":0,"endNumber":500,"totalNumber":5000,"decimalPlaces":0,"animationDuration":1500,"thousandSeparator":",","circleSize":230,"circleStokeSize":8,"isFrontend":true} );
+});
+window.addEventListener( 'load', function() {
+	UAGBCounter.init( '.uagb-block-db7e65fc', {"layout":"number","heading":"Homes Built","numberPrefix":"","numberSuffix":"+","startNumber":0,"endNumber":200,"totalNumber":5000,"decimalPlaces":0,"animationDuration":1500,"thousandSeparator":",","circleSize":230,"circleStokeSize":8,"isFrontend":true} );
+});
+ });
